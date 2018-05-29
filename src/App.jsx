afdfa42933ff3d310f9590660dbb42b7b3b83197
Walter Shub <waltershub@gmail.com>
2018-05-28 22:24:17 -0400

    ready for trail

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Charity Giving</h1>
+          <h1 className="App-title">Charity Miner(BETA)</h1>
         </header>
         <div className="body">
           <div className="dropdown">
